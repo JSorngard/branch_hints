a651302c0a80be97a4ebd3df603d28a114470b3b
Johanna Sörngård <jsorngard@gmail.com>
2022-12-22 14:24:06 +0100

    Add crate docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 //! This crate provides [likely] and [unlikely] functions that work as compiler hints for branching.
 //! They are taken directly from the [hashbrown](https://crates.io/crates/hashbrown) crate, all credit belongs to them.
+//! These functions enable constructions like
+//! ```
+//! # use branch_hints::likely;
+//! # let condition = true;
+//! if likely(condition) {
+//!     // main code
+//! } else {
+//!     // error handling code
+//! }
+//! ```
 
 // Branch prediction hint. This is currently only available on nightly.
 #[cfg(feature = "nightly")]
 use core::intrinsics::{likely, unlikely};
 
 // On stable we can use #[cold] to get a equivalent effect: this attributes
-// suggests that the function is unlikely to be called
+// suggests that the function is unlikely to be called. This function should
+// be optimized away by the compiler.
 #[cfg(not(feature = "nightly"))]
 #[inline]
 #[cold]
